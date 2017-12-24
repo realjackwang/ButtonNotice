149d69e7cf580f9cebf9e93234fbfec900343680
skycity233 <544907049@qq.com>
2017-12-24 23:55:53 +0800

    2017/12/24 20:27

--- app/src/main/java/com/notice/button/button/service/Constant.java
+++ app/src/main/java/com/notice/button/button/service/Constant.java
@@ -9,7 +9,7 @@ public class Constant {
     public static String URL_Register = URL + "RegisterServlet" ;
     public static String URL_Login = URL + "LoginServlet" ;
     public static String URL_Create = URL + "CreateServlet" ;
-    public static String URL_User = URL + "UserServlet" ;
+    public static String URL_Delete = URL + "DeleteServlet" ;
     public static String URL_Updata = URL + "UpdataServlet";
     public static String URL_Query = URL + "QueryServlet";
     public static int HANDLER_HTTP_RECEIVE_FAIL =0;
