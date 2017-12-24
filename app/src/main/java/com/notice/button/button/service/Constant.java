639dafe6fbb60b1c9f3e5c4052312ce1ecad5063
skycity233 <544907049@qq.com>
2017-12-24 22:43:50 +0800

    2017/12/24 20:27

--- app/src/main/java/com/notice/button/button/service/Constant.java
+++ app/src/main/java/com/notice/button/button/service/Constant.java
@@ -8,6 +8,7 @@ public class Constant {
     public static String URL = "http://172.25.146.152/ServletTest/"; // IP地址请改为你自己的IP
     public static String URL_Register = URL + "RegisterServlet" ;
     public static String URL_Login = URL + "LoginServlet" ;
+    public static String URL_Create = URL + "CreateServlet" ;
     public static String URL_User = URL + "UserServlet" ;
     public static String URL_Updata = URL + "UpdataServlet";
     public static String URL_Query = URL + "QueryServlet";
