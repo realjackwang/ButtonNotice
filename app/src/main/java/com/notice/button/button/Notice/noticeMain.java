4afc69ee02543c375051869b589ef73dfdcf0832
Jill0001 <1465514267@qq.com>
2017-12-25 21:37:54 +0800

    .

--- app/src/main/java/com/notice/button/button/Notice/noticeMain.java
+++ app/src/main/java/com/notice/button/button/Notice/noticeMain.java
@@ -3,13 +3,17 @@ package com.notice.button.button.Notice;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.notice.button.button.Button.buttonLogin;
 import com.notice.button.button.R;
 import com.notice.button.button.User.userInfo;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +27,9 @@ public class noticeMain extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_main);
+        final Button btnLogin = (Button) findViewById(R.id.login);
+
+
 
         initNotice();//初始化通知数据
 
@@ -49,6 +56,7 @@ public class noticeMain extends Activity {
         noticeDetail no1 = new noticeDetail("Apple", "11.2", "fsef", "2e", "gvsdvg");
         notice_list.add(no1);
     }
+
 
 
 }
