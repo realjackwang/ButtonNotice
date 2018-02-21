19cd3dd1de10d7ef4a01a8857690769ffdf6eead
Jill0001 <1465514267@qq.com>
2018-02-21 23:17:12 +0800

    .

--- app/src/main/java/com/button/notice/Notice/noticeDetailActivity.java
+++ app/src/main/java/com/button/notice/Notice/noticeDetailActivity.java
@@ -1,12 +1,14 @@
 package com.button.notice.Notice;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.renderscript.Int2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.button.notice.Fragment.MainActivity;
 import com.button.notice.Fragment.noticeFragment;
@@ -33,22 +35,33 @@ public class noticeDetailActivity extends AppCompatActivity {
         Button collect = findViewById(R.id.collect);
         Button read = findViewById(R.id.read);
 
+        //**********************//
+        // 返回按钮点击事件
         back.setOnClickListener((view -> {
-            Intent intent = new Intent(noticeDetailActivity.this,MainActivity.class);
-            startActivity(intent);
+           finish();
         })
         );
+        //**********************//
 
+
+        //**********************//
+        //问询按钮点击跳转页面
         query.setOnClickListener((view -> {
             Intent intent = new Intent(noticeDetailActivity.this,noticeDetialQA.class);
             startActivity(intent);
         }));
+        //**********************//
 
+        //**********************//
+        //通知详情内容
         TextView title = (findViewById(R.id.noticeTitle));
         TextView author = (findViewById(R.id.Author));
         TextView createTime = (findViewById(R.id.noticeCreateTime));
         TextView text = (findViewById(R.id.Text));
+        //**********************//
 
+        //**********************//
+        //intent传值 查询数据库 获取点击的通知的内容
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
         CommonRequest request1 = new CommonRequest();
@@ -68,15 +81,29 @@ public class noticeDetailActivity extends AppCompatActivity {
                 createTime.setText("发布日期："+CreateDate);
                 author.setText("作者："+Author);
                 text.setText("内容："+Text);
-
             }
 
             @Override
             public void fail(String failCode, String failMsg) {
+                Toast.makeText(noticeDetailActivity.this, "加载失败 请重试", Toast.LENGTH_SHORT).show();
 
             }
         });
+        //**********************//
+
+        //**********************//
+        //收藏
+        collect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CommonRequest request = new CommonRequest();
+
+
+
+            }
+        });
+
+
+
     }
-
-
 }
